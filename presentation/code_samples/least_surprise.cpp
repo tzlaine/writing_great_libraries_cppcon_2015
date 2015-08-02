f590b7683bd4ae837b9e0565b63b7244d43d008d
Zach Laine <whatwasthataddress@gmail.com>
2015-08-02 11:19:42 -0500

    Add section on Principle of Least Surprise.

---
+++ presentation/code_samples/least_surprise.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <vector>
+
+// sample(unexpected_delete)
+struct X
+{/*...*/};
+
+void RegisterX(X const * x);
+// end-sample
+
+int main()
+{
+
+// sample(unexpected_delete)
+X * x = new X{/*...*/};
+RegisterX(x);
+// end-sample
+
+    return 0;
+}
+
+// sample(register_x_defn)
+void RegisterX(X const * x)
+{
+    if (x) {
+        // Some stuff that "registers" x here...
+    }
+    delete x; // WTF
+}
+// end-sample
